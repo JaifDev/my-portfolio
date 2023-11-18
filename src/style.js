cd88e58bf7ae2fffe0b0888fec83643c05d5ae26
Jaif <jaifm5525@gmail.com>
2023-11-18 23:40:59 +0530

    first commit

---
+++ src/style.js
@@ -0,0 +1,17 @@
+const styles = {
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+  heroHeadText:
+    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+  heroSubText:
+    "text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[20px] xs:text-[18px] text-[16px] lg:leading-[40px]",
+
+  sectionHeadText:
+    "text-white font-black md:text-[55px] sm:text-[45px] xs:text-[35px] text-[25px]",
+  sectionSubText:
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+};
+
+export { styles };
